1d312a603e7229afd782a5e320cfe8849bdefccd
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-10-27 22:49:13 +0900

    css module 처리

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button.js";
+import styled from "./App.module.css";
+
 function App() {
   return (
     <div className="App">
-      <h1>리엑트 ㅎㅇ ㅋ</h1>
+      <h1 className={styled.title}>리엑트 ㅎㅇ ㅋ</h1>
+      <Button text={"버튼을 눌러라 !!!"} />
     </div>
   );
 }

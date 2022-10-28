525ee7ad840a3ed2f2dfd087e39c5baebed2d3af
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-10-28 21:58:25 +0900

    React로 Todo Part 1

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,21 +2,26 @@ import Button from "./Button.js";
 import styled from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("hi");
-    return () => console.log("bye");
-  }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (e) => setTodo(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setTodo("");
+    setTodos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "hide" : "Show"}</button>
+      <h1>나의 작업 ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="할일을 적어주세요" />
+        <button>할일 추가</button>
+      </form>
     </div>
   );
 }

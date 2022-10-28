ab27f3d4673d6ea6591007aa67e29ca9e0b6b9df
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-10-28 23:01:38 +0900

    Coin Tracker

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -3,31 +3,44 @@ import styled from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setTodo] = useState("");
-  const [toDos, setTodos] = useState([]);
-  const onChange = (e) => setTodo(e.target.value);
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setTodo("");
-    setTodos((currentArray) => [toDo, ...currentArray]);
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    const COIN_URL = "https://api.coinpaprika.com/v1/tickers";
+    fetch(COIN_URL)
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>나의 작업 ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="할일을 적어주세요" />
-        <button>할일 추가</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, i) => (
-          <li key={i}>{item}</li>
-        ))}
-      </ul>
+      <h1>코인 리스트 {loading ? "" : `(${coins.length})`} </h1>
+      {loading ? <strong>로딩 중...</strong> : <SelectCoins coins={coins} />}
+    </div>
+  );
+}
+
+function SelectCoins({ coins }) {
+  const [btc, setBtc] = useState(0);
+  const onChange = (e) => {
+    const value = e.target.value / 20501.39;
+    setBtc(value.toFixed());
+  };
+  return (
+    <div>
+      <div>
+        <select>
+          {coins.map((item, i) => (
+            <option key={i}>
+              {item.rank} / {item.name} / ({item.symbol}): {item.quotes.USD.price}
+            </option>
+          ))}
+        </select>
+      </div>
+      <input onChange={onChange} type="text" placeholder="가진 돈을 적어보세요" />
+      <input value={btc} type="text" placeholder="BTC" disabled />
     </div>
   );
 }

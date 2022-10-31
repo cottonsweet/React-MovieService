1681a7be7d96e08d5af0ae2cc2012a7f14639c44
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-10-31 21:35:23 +0900

    React Movie App part One

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,46 +1,39 @@
-import Button from "./Button.js";
-import styled from "./App.module.css";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    const COIN_URL = "https://api.coinpaprika.com/v1/tickers";
-    fetch(COIN_URL)
-      .then((res) => res.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  return (
-    <div>
-      <h1>코인 리스트 {loading ? "" : `(${coins.length})`} </h1>
-      {loading ? <strong>로딩 중...</strong> : <SelectCoins coins={coins} />}
-    </div>
-  );
-}
-
-function SelectCoins({ coins }) {
-  const [btc, setBtc] = useState(0);
-  const onChange = (e) => {
-    const value = e.target.value / 20501.39;
-    setBtc(value.toFixed(5));
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year");
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
   return (
     <div>
-      <div>
-        <select>
-          {coins.map((item, i) => (
-            <option key={i}>
-              {item.rank} / {item.name} / ({item.symbol}): {item.quotes.USD.price}
-            </option>
+      {loading ? (
+        <h1>로딩중입니다...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image}></img>
+              <h2> {movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
           ))}
-        </select>
-      </div>
-      <input onChange={onChange} type="text" placeholder="가진 돈을 적어보세요" />
-      <input value={btc} type="text" placeholder="BTC" disabled />
+        </div>
+      )}
     </div>
   );
 }
